de0a490cfaefaaab1fbd32654cfa424138f4382d
André Lademann <alademann@thinkport.digital>
2024-03-12 17:14:01 +0100

    feat: Separate functions

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -2,11 +2,7 @@
 
 void setup()
 {
-    Serial.begin(115200); // Start serial communication with 115200 baud
-    // Wait for the serial monitor to open
-    while (!Serial)
-        ;
-    Serial.println("Setup done"); // Print a message to the serial console
+    Serial.begin(115200);
 }
 
 void loop()
@@ -15,7 +11,7 @@ void loop()
     {                                               // Check whether data is available
         String data = Serial.readStringUntil('\n'); // Read data up to the line break
         Serial.println("Echo: " + data);            // Send back the received data
-        Serial.write(0x04);
-        Serial.end();
+        // Serial.write(0x04);
+        // Serial.end();
     }
 }
